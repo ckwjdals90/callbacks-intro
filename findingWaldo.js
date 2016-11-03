950a6b82aec39d73657847f53b1de7ab0df2fb39
Jungmin Cha <ckwjdals90@gmail.com>
2016-11-03 18:43:32 +0000

    Added findingWaldo.js

---
+++ findingWaldo.js
@@ -0,0 +1,14 @@
+
+function findWaldo(arr, found) {
+  arr.forEach(function (element, i) {
+    if (element === "Waldo") {
+      found(i);   // execute callback
+    }
+  });
+}
+
+function actionWhenFound(index) {
+  console.log("Found Waldo at index", index + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
